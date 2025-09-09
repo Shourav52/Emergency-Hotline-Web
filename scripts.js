acd140948e2ff9ba1fd53248c141dae0a785aec7
Shourav52 <shorovsaha52@gmail.com>
2025-09-09 03:38:07 -0700

    added call button and clear button functionality

--- scripts.js
+++ scripts.js
@@ -31,3 +31,46 @@ for(const copyBtn of copyBtns){
     
     });
 }
+
+
+const callBtns = document.querySelectorAll(".call-btn");
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", function (event) {
+    const card = event.target.closest(".cards-section");
+    const cardTitle = card.querySelector(".card-title").textContent;
+    const callNbrs = card.querySelector(".call-nbr").textContent;
+
+    if (coinCount < 20) {
+      alert("Not enough your coins!");
+
+    return;
+    }
+  coinCount = coinCount -20;
+    coinIcon.textContent = coinCount;
+    alert("Calling " + cardTitle + " at " + callNbrs);
+    const now = new Date();
+    const timeValue = now.toLocaleString();
+
+    const li = document.createElement("li");
+    li.innerHTML =
+      "<span><strong>" +
+      cardTitle +
+      "</strong> - " +
+      callNbrs +
+      "</span> <span>" +
+      timeValue +
+      "</span>";
+
+      historyList.appendChild( li);
+
+   });
+
+}
+clearIcon.addEventListener("click", function () {
+  const allClear = confirm("Are you sure?");
+  if (allClear) {
+    historyList.innerHTML = "";
+
+  } 
+
+});
